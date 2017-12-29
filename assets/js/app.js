95e28ea7e2a67f6073582ba81cedb5db9bef7637
Laboratoria178 <cruizcornejo@gmail.com>
2017-12-29 10:21:19 -0300

    funcionalidad

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,45 @@
-var close = document.getElementsByClassName("addBtn");
-var i;
-for (i = 0; i < addBtn.length; i++) {
-  addBtn[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+//Variables.
+var btnUno = document.getElementById('btnAdd');
+var input =  document.getElementsByClassName('insetdos')[0];
+var btnDos = document.getElementByClassName('btnSave');
+var close = document.getElementByClassName('close');
+var cont = document.getElementById('insettres');
+
+//Evento click, para cambiar de boton a input.
+btnUno.addEventListener('click', function(){
+	btnUno.classList.add('ocult-inset');
+	input.classList.remove('ocult-inset');
+});
+
+// Evento close, para cambiar de input a boton.
+
+close.addEventListener('click', function(){
+	input.classList.add('ocult-inset');
+	btnUno.classList.remove('ocult-inset');
+});
+
+/// Agregando lista
+btnDos.addEventListener('click', function(){
+	cont.classList.remove('ocult-inset');
+//Tomar el texto que ingresara el usuario
+var text = document.getElementById('text').value;
+text.textContent = text; //escribe el contenido de la lista
+//limpiar input
+document.getElementById('text').value = '';
+
+
+//Creando Nodos
+
+var textN = document.createTextNode(text);
+var newList = document.createElement('div');
+var textList = document.createElement('p');
+
+//Orden de Hijos
+
+textList.appendChild(textN);
+newList.appendChild(textList);
+cont2.appendChild(newList);
+
+
+
+});
